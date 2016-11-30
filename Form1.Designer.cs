00cb6b34030388b31cab80c95d80924f16efdd61
thunyalak <tthunyalak@kkumail.com>
2016-11-30 11:33:19 +0700

    Add files via upload
    คำอธิบายโครงงานอย่างย่อ : โปรแกรมนี้ใช้เครื่องมือ Month Calendar ในการเลือกวันที่ หรือเดือน เพื่อแสดงรูปภพคำทำนายของราศีต่าง ๆ ทั้ง 12 ราศี

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ทำนายนิสัยจาก 12 ราศี";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(16, 109);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 7;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(216, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 361);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        int d, m;
+        public Form1()
+        {
+            InitializeComponent();
+            DateTime date = monthCalendar1.SelectionEnd;
+            d = date.Day; // d คือตัวแปร เก็บค่าของวันที่
+            m = date.Month; // m คือตัวแปร เก็บค่าของเดือน
+            if ((m == 1 && d >= 16) || (m == 2 && d <= 12)) // เงื่อนไขนี้คือ เดือนที่1 ตั้งแต่วันที่มากกว่าหรือเท่ากับ16ขึ้นไปจนถึง เดือนที่2 จนถึงวันที่น้อยกว่าหรือเท่ากับ12
+            {
+                pictureBox1.Image = Properties.Resources.มังกร; // ที่อยู่ของรูปภาพราศีมังกร 
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีมังกร
+            }
+            else
+            if ((m == 2 && d >= 13) || (m == 3 && d <= 13)) // เงื่อนไขนี้คือ เดือนที่2 ตั้งแต่วันที่มากกว่าหรือเท่ากับ13ขึ้นไปจนถึง เดือนที่3 จนถึงวันที่น้อยกว่าหรือเท่ากับ13
+            {
+                pictureBox1.Image = Properties.Resources.กุมภ์;// ที่อยู่ของรูปภาพราศีกุมภ์
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีกุมภ์
+            }
+            else
+          if ((m == 3 && d >= 14) || (m == 4 && d <= 12)) // เงื่อนไขนี้คือ เดือนที่3 ตั้งแต่วันที่มากกว่าหรือเท่ากับ14ขึ้นไปจนถึง เดือนที่4 จนถึงวันที่น้อยกว่าหรือเท่ากับ12
+            {
+                pictureBox1.Image = Properties.Resources.มีน;// ที่อยู่ของรูปภาพราศีมีน
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีมีน
+            }
+            else
+          if ((m == 4 && d >= 13) || (m == 5 && d <= 13)) // เงื่อนไขนี้คือ เดือนที่4 ตั้งแต่วันที่มากกว่าหรือเท่ากับ13ขึ้นไปจนถึง เดือนที่5 จนถึงวันที่น้อยกว่าหรือเท่ากับ13
+            {
+                pictureBox1.Image = Properties.Resources.เมษ;// ที่อยู่ของรูปภาพราศีเมษ
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีเมษ
+            }
+            else
+          if ((m == 5 && d >= 14) || (m == 6 && d <= 13)) // เงื่อนไขนี้คือ เดือนที่5 ตั้งแต่วันที่มากกว่าหรือเท่ากับ14ขึ้นไปจนถึง เดือนที่6 จนถึงวันที่น้อยกว่าหรือเท่ากับ13
+            {
+                pictureBox1.Image = Properties.Resources.พฤษภ;// ที่อยู่ของรูปภาพราศีพฤษภ
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีพฤษภ
+            }
+            else
+          if ((m == 6 && d >= 14) || (m == 7 && d <= 14)) // เงื่อนไขนี้คือ เดือนที่6 ตั้งแต่วันที่มากกว่าหรือเท่ากับ14ขึ้นไปจนถึง เดือนที่7 จนถึงวันที่น้อยกว่าหรือเท่ากับ14
+            {
+                pictureBox1.Image = Properties.Resources.เมถุน;// ที่อยู่ของรูปภาพราศีเมถุน
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีเมถุน
+            }
+            else
+          if ((m == 7 && d >= 15) || (m == 8 && d <= 16)) // เงื่อนไขนี้คือ เดือนที่7 ตั้งแต่วันที่มากกว่าหรือเท่ากับ15ขึ้นไปจนถึง เดือนที่8 จนถึงวันที่น้อยกว่าหรือเท่ากับ16
+            {
+                pictureBox1.Image = Properties.Resources.กรกฎ;// ที่อยู่ของรูปภาพราศีกรกฏ
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีกรกฏ
+            }
+            else
+          if ((m == 8 && d >= 17) || (m == 9 && d <= 16)) // เงื่อนไขนี้คือ เดือนที่8 ตั้งแต่วันที่มากกว่าหรือเท่ากับ17ขึ้นไปจนถึง เดือนที่9 จนถึงวันที่น้อยกว่าหรือเท่ากับ16
+            {
+                pictureBox1.Image = Properties.Resources.สิงห์;// ที่อยู่ของรูปภาพราศีสิงห์
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีสิงห์
+            }
+            else
+          if ((m == 9 && d >= 17) || (m == 10 && d <= 16)) // เงื่อนไขนี้คือ เดือนที่9 ตั้งแต่วันที่มากกว่าหรือเท่ากับ17ขึ้นไปจนถึง เดือนที่10 จนถึงวันที่น้อยกว่าหรือเท่ากับ16
+            {
+                pictureBox1.Image = Properties.Resources.กันย์;// ที่อยู่ของรูปภาพราศีกันย์
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีกันย์
+            }
+            else
+          if ((m == 10 && d >= 17) || (m == 11 && d <= 15)) // เงื่อนไขนี้คือ เดือนที่10 ตั้งแต่วันที่มากกว่าหรือเท่ากับ17ขึ้นไปจนถึง เดือนที่11 จนถึงวันที่น้อยกว่าหรือเท่ากับ15
+            {
+                pictureBox1.Image = Properties.Resources.ตุลย์;// ที่อยู่ของรูปภาพราศีตุลย์
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีตุลย์
+            }
+            else
+          if ((m == 11 && d >= 16) || (m == 12 && d <= 15)) // เงื่อนไขนี้คือ เดือนที่11 ตั้งแต่วันที่มากกว่าหรือเท่ากับ16ขึ้นไปจนถึง เดือนที่12 จนถึงวันที่น้อยกว่าหรือเท่ากับ15
+            {
+                pictureBox1.Image = Properties.Resources.พิจิก;// ที่อยู่ของรูปภาพราศีพิจิก
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีพิจิก
+            }
+            else
+          if ((m == 12 && d >= 16) || (m == 1 && d <= 15)) // เงื่อนไขนี้คือ เดือนที่12 ตั้งแต่วันที่มากกว่าหรือเท่ากับ16ขึ้นไปจนถึง เดือนที่1 จนถึงวันที่น้อยกว่าหรือเท่ากับ15
+            {
+                pictureBox1.Image = Properties.Resources.ธนู;// ที่อยู่ของรูปภาพราศีธนู
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // ขนาดของรูปภาพราศีธนู
+            }
+            pictureBox1.Visible = true;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            DateTime date = monthCalendar1.SelectionEnd;
+            d = date.Day;// d คือตัวแปร เก็บค่าของวันที่
+            m = date.Month;// m คือตัวแปร เก็บค่าของเดือน
+            if ((m == 1 && d >= 16) || (m == 2 && d <= 12)) // เงื่อนไขนี้คือ เดือนที่1 ตั้งแต่วันที่มากกว่าหรือเท่ากับ16ขึ้นไปจนถึง เดือนที่2 จนถึงวันที่น้อยกว่าหรือเท่ากับ12
+            {
+                pictureBox1.Image = Properties.Resources.มังกร;// ที่อยู่ของรูปภาพราศีมังกร
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีมังกร
+            }
+            else
+             if ((m == 2 && d >= 13) || (m == 3 && d <= 13))// เงื่อนไขนี้คือ เดือนที่2 ตั้งแต่วันที่มากกว่าหรือเท่ากับ13ขึ้นไปจนถึง เดือนที่3 จนถึงวันที่น้อยกว่าหรือเท่ากับ13
+            {
+                pictureBox1.Image = Properties.Resources.กุมภ์;// ที่อยู่ของรูปภาพราศีกุมภ์
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีกุมภ์
+            }
+            else
+           if ((m == 3 && d >= 14) || (m == 4 && d <= 12))// เงื่อนไขนี้คือ เดือนที่3 ตั้งแต่วันที่มากกว่าหรือเท่ากับ14ขึ้นไปจนถึง เดือนที่4 จนถึงวันที่น้อยกว่าหรือเท่ากับ12
+            {
+                pictureBox1.Image = Properties.Resources.มีน;// ที่อยู่ของรูปภาพราศีมีน
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีมีน
+            }
+            else
+           if ((m == 4 && d >= 13) || (m == 5 && d <= 13))// เงื่อนไขนี้คือ เดือนที่4 ตั้งแต่วันที่มากกว่าหรือเท่ากับ13ขึ้นไปจนถึง เดือนที่5 จนถึงวันที่น้อยกว่าหรือเท่ากับ13
+            {
+                pictureBox1.Image = Properties.Resources.เมษ;// ที่อยู่ของรูปภาพราศีเมษ
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีเมษ
+            }
+            else
+           if ((m == 5 && d >= 14) || (m == 6 && d <= 13))// เงื่อนไขนี้คือ เดือนที่5 ตั้งแต่วันที่มากกว่าหรือเท่ากับ14ขึ้นไปจนถึง เดือนที่6 จนถึงวันที่น้อยกว่าหรือเท่ากับ13
+            {
+                pictureBox1.Image = Properties.Resources.พฤษภ;// ที่อยู่ของรูปภาพราศีพฤษภ
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีพฤษภ
+            }
+            else
+           if ((m == 6 && d >= 14) || (m == 7 && d <= 14)) // เงื่อนไขนี้คือ เดือนที่6 ตั้งแต่วันที่มากกว่าหรือเท่ากับ14ขึ้นไปจนถึง เดือนที่7 จนถึงวันที่น้อยกว่าหรือเท่ากับ14
+            {
+                pictureBox1.Image = Properties.Resources.เมถุน;// ที่อยู่ของรูปภาพราศีเมถุน
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีเมถุน
+            }
+            else
+           if ((m == 7 && d >= 15) || (m == 8 && d <= 16)) // เงื่อนไขนี้คือ เดือนที่7 ตั้งแต่วันที่มากกว่าหรือเท่ากับ15ขึ้นไปจนถึง เดือนที่8 จนถึงวันที่น้อยกว่าหรือเท่ากับ16
+            {
+                pictureBox1.Image = Properties.Resources.กรกฎ;// ที่อยู่ของรูปภาพราศีกรกฏ
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีกรกฏ
+            }
+            else
+           if ((m == 8 && d >= 17) || (m == 9 && d <= 16))// เงื่อนไขนี้คือ เดือนที่8 ตั้งแต่วันที่มากกว่าหรือเท่ากับ17ขึ้นไปจนถึง เดือนที่9 จนถึงวันที่น้อยกว่าหรือเท่ากับ16
+            {
+                pictureBox1.Image = Properties.Resources.สิงห์;// ที่อยู่ของรูปภาพราศีสิงห์
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีสิงห์
+            }
+            else
+           if ((m == 9 && d >= 17) || (m == 10 && d <= 16))// เงื่อนไขนี้คือ เดือนที่9 ตั้งแต่วันที่มากกว่าหรือเท่ากับ17ขึ้นไปจนถึง เดือนที่10 จนถึงวันที่น้อยกว่าหรือเท่ากับ16
+            {
+                pictureBox1.Image = Properties.Resources.กันย์;// ที่อยู่ของรูปภาพราศีกันย์
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีกันย์
+            }
+            else
+           if ((m == 10 && d >= 17) || (m == 11 && d <= 15))// เงื่อนไขนี้คือ เดือนที่10 ตั้งแต่วันที่มากกว่าหรือเท่ากับ17ขึ้นไปจนถึง เดือนที่11 จนถึงวันที่น้อยกว่าหรือเท่ากับ15
+            {
+                pictureBox1.Image = Properties.Resources.ตุลย์;// ที่อยู่ของรูปภาพราศีตุลย์
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีตุลย์
+            }
+            else
+           if ((m == 11 && d >= 16) || (m == 12 && d <= 15))// เงื่อนไขนี้คือ เดือนที่11 ตั้งแต่วันที่มากกว่าหรือเท่ากับ16ขึ้นไปจนถึง เดือนที่12 จนถึงวันที่น้อยกว่าหรือเท่ากับ15
+            {
+                pictureBox1.Image = Properties.Resources.พิจิก;// ที่อยู่ของรูปภาพราศีพิจิก
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีพิจิก
+            }
+            else
+           if ((m == 12 && d >= 16) || (m == 1 && d <= 15))// เงื่อนไขนี้คือ เดือนที่12 ตั้งแต่วันที่มากกว่าหรือเท่ากับ16ขึ้นไปจนถึง เดือนที่1 จนถึงวันที่น้อยกว่าหรือเท่ากับ15
+            {
+                pictureBox1.Image = Properties.Resources.ธนู;// ที่อยู่ของรูปภาพราศีธนู
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;// ขนาดของรูปภาพราศีธนู
+            }
+            pictureBox1.Refresh();
+        }
+
+    }
+}
